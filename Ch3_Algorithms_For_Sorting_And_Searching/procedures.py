274effc42eba0f31898059e7bec24849d55be97d
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-05-18 15:46:23 -0700

    Implement and test Binary Search

--- Ch3_Algorithms_For_Sorting_And_Searching/procedures.py
+++ Ch3_Algorithms_For_Sorting_And_Searching/procedures.py
@@ -1,20 +1,24 @@
-def linear_search(A, n, x):
+def binary_search(A, n, x):
     """
-    Use linear search to find a value in an array.
+    Use binary search to find a value in a sorted array.
     
     Args:
-        A: any array.
+        A: any sorted array.
         n: the number of elements in A to search through.
         x: the value being searched for.
     Returns:
         the index of an element that is equal to x (zero-based), or the string "NOT-FOUND"
         to show that x is not in the array.
     """
-    i = 0
-    index = "NOT-FOUND"
-    while i < n:
-        if A[i] == x:
-            index = i
-        i += 1
-    return index
+    p = 0
+    r = n - 1
+    while p <= r:
+        q = (p + r) // 2
+        if A[q] == x:
+            return q
+        elif A[q] > x:
+            r = q - 1
+        elif A[q] < x:
+            p = q + 1
+    return "NOT-FOUND"
 
