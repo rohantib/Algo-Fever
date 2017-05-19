eac534987a30b9bbca4823105fbfea2b31be7b53
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-05-18 22:34:51 -0700

    Implement and test Recursive Binary Search

--- Ch3_Algorithms_For_Sorting_And_Searching/procedures.py
+++ Ch3_Algorithms_For_Sorting_And_Searching/procedures.py
@@ -22,3 +22,27 @@ def binary_search(A, n, x):
             p = q + 1
     return "NOT-FOUND"
 
+
+def recursive_binary_search(A, p, r, x):
+    """
+    Use binary search to find a value in a sorted array.
+
+    Args:
+        A: any sorted array.
+        p: the start of the subarray of A (zero-based)
+        r: the end of hte subarray of A (zero-based)
+        x: the value being searched for.
+    Returns:
+        the index of an element that is equal to x (zero-based), or the string "NOT-FOUND"
+        to show that x is not in the array.
+    """
+    if p > r:
+        return "NOT-FOUND"
+    q = (p + r) // 2
+    if A[q] == x:
+        return q
+    elif A[q] > x:
+        return recursive_binary_search(A, p, q - 1, x)
+    elif A[q] < x:
+        return recursive_binary_search(A, q + 1, r, x)
+
